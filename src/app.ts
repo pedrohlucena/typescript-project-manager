7ac9256770fffc93d1f4101e491c6a5ce0e52754
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-10-17 17:22:55 -0300

    AutoBind decorator implementation

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,42 @@
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value
+    console.log(descriptor)
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFunction = originalMethod.bind(this)
+            return boundFunction
+        }
+    }
+    console.log(adjDescriptor)
+    return adjDescriptor
+}
 class ProjectInput {
     static instance: ProjectInput
+
     templateElement: HTMLTemplateElement
     hostElement: HTMLDivElement
-    element: HTMLFormElement
+
+    formElement: HTMLFormElement
+
+    titleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
+    peopleInputElement: HTMLInputElement
 
     private constructor() {
         this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!
         this.hostElement = <HTMLDivElement>document.getElementById('app')! 
 
         const importedNode = document.importNode(this.templateElement.content, true)
-        this.element = importedNode.firstElementChild as HTMLFormElement 
-        this.element.id = 'user-input'
+        this.formElement = importedNode.firstElementChild as HTMLFormElement 
+        this.formElement.id = 'user-input'
+
+        this.titleInputElement = this.formElement.querySelector('#title') as HTMLInputElement
+        this.descriptionInputElement = this.formElement.querySelector('#description') as HTMLInputElement
+        this.peopleInputElement = this.formElement.querySelector('#people') as HTMLInputElement
+
+        this.configure()
         this.attach()
     }
 
@@ -23,7 +49,17 @@ class ProjectInput {
     }
 
     private attach() {
-        this.hostElement.insertAdjacentElement('afterbegin', this.element)
+        this.hostElement.insertAdjacentElement('afterbegin', this.formElement)
+    }
+
+    @AutoBind
+    private submitHandler(event: Event) {
+        event.preventDefault()
+        console.log(this.titleInputElement.value)
+    }
+
+    private configure() {
+        this.formElement.addEventListener('submit', this.submitHandler)
     }
 }
 
