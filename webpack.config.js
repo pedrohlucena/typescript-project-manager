e64d1c0cf8527f238decaf1f4b7e8fea9e17bc94
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-11-11 18:20:51 -0300

    finish webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,24 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-    entry: './src/app.ts',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    }
-}
+  mode: 'development',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+};
